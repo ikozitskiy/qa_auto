36f5a69bbcb605e9e62de07c91add9f350aacb5c
igor <ikozitskiu@gmail.com>
2024-05-28 22:17:05 +0300

    added api tests

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from modules.api.clients.github import GitHub
 class User:
     def __init__(self) -> None:
         self.name = None
@@ -21,3 +22,9 @@ def user():
     yield user
 
     user.remove()
+
+
+@pytest.fixture
+def github_api():
+    api = GitHub()
+    yield api
